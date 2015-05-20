fb7fac60c3544e6ca17ae6d02d5e76a1a07f99d2
Philipp Muens <raute1337@gmx.de>
2015-05-20 13:41:28 +0200

    Update asset-Links
    Update asset-Links so that the newest versions of the files are used.

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'goltfisch:ziggeo',
   summary: 'Ziggeo library for Meteor',
-  version: '0.1.0',
+  version: '0.1.1',
   git: 'https://github.com/goltfisch/meteor-ziggeo.git'
 });
 
